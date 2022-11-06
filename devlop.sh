61a5788e0f05fd0be952c1402922b74dad30fb80
chandra <kcr204@gmail.com>
2022-11-06 15:10:59 +0530

    guru

--- devlop.sh
+++ devlop.sh
@@ -6,9 +6,7 @@ refs/remotes/origin/main
 chanda reddy kanala
 we  are good to start
 we hope we are good
-<<<<<<< HEAD
 I have removed github credentials
 Welcome to git conflits
-=======
 I introduced to git stash
->>>>>>> 0f9e64457d6485adc6f713cbc3f854cb4175379b
+Welocme to git stash list
--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@ welcome to conflits
 welcome to git
 welcome to got statsh
 welcome to conflitcs
+welcome to statsh
